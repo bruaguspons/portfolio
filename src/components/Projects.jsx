0e784afdebc780e94b91f36f7dc1d7cd4331760a
Bruno <bruaguspons@gmail.com>
2022-11-15 09:15:02 -0300

    space-content

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -10,55 +10,58 @@ import Blog from './../assets/Blog.png'
 const Projects = () => {
     return (
         <div id='projects' className='reveal'>
-            <div className='cards-content'>
+            <div className='card-title-content'>
+
                 <h2 className='title'>Projects</h2>
-                <div className='card'>
-                    <div className='singleCard'>
+                <div className='cards-content'>
+                    <div className='card'>
+                        <div className='singleCard'>
 
-                        <h3>Social Media App</h3>
-                        <div className='image'>
-                            <a href="https://github.com/bruaguspons/Social-Media-React-Express" target='_blank'>
-                                <img src={SocialMedia} alt="Social Media" />
-                            </a>
+                            <h3>Social Media App</h3>
+                            <div className='image'>
+                                <a href="https://github.com/bruaguspons/Social-Media-React-Express" target='_blank'>
+                                    <img src={SocialMedia} alt="Social Media" />
+                                </a>
+                            </div>
+                            <p>Made With:</p>
+                            <div className='tecs'>
+                                <div className='icon'>
+                                    <img src={mongodb} alt="Mongodb" />
+                                    <span>Mongodb</span>
+                                </div>
+                                <div className='icon'>
+                                    <img src={express} alt="Express" />
+                                    <span>Express</span>
+                                </div>
+                                <div className='icon'>
+                                    <img src={reactLogo} alt="React" />
+                                    <span>React</span>
+                                </div>
+                            </div>
+                            <a href='https://github.com/bruaguspons/Social-Media-React-Express' target="_blank">More Info</a>
                         </div>
-                        <p>Made With:</p>
-                        <div className='tecs'>
-                            <div className='icon'>
-                                <img src={mongodb} alt="Mongodb" />
-                                <span>Mongodb</span>
-                            </div>
-                            <div className='icon'>
-                                <img src={express} alt="Express" />
-                                <span>Express</span>
-                            </div>
-                            <div className='icon'>
-                                <img src={reactLogo} alt="React" />
-                                <span>React</span>
-                            </div>
-                        </div>
-                        <a href='https://github.com/bruaguspons/Social-Media-React-Express' target="_blank">More Info</a>
                     </div>
-                </div>
-                <div className='card'>
-                    <div className='singleCard'>
+                    <div className='card'>
+                        <div className='singleCard'>
 
-                        <h3>Blog-Site</h3>
-                        <div className='image'>
-                            <a href="https://github.com/bruaguspons/Blog-Django-React" target='_blank'>
-                                <img src={Blog} alt="Blog" />
-                            </a>
-                        </div>
-                        <div className='tecs'>
-                            <div className='icon'>
-                                <img src={django} alt="Django" />
-                                <span>Django</span>
+                            <h3>Blog-Site</h3>
+                            <div className='image'>
+                                <a href="https://github.com/bruaguspons/Blog-Django-React" target='_blank'>
+                                    <img src={Blog} alt="Blog" />
+                                </a>
                             </div>
-                            <div className='icon'>
-                                <img src={reactLogo} alt="React" />
-                                <span>React</span>
+                            <div className='tecs'>
+                                <div className='icon'>
+                                    <img src={django} alt="Django" />
+                                    <span>Django</span>
+                                </div>
+                                <div className='icon'>
+                                    <img src={reactLogo} alt="React" />
+                                    <span>React</span>
+                                </div>
                             </div>
+                            <a href='https://github.com/bruaguspons/Blog-Django-React' target="_blank">More Info</a>
                         </div>
-                        <a href='https://github.com/bruaguspons/Blog-Django-React' target="_blank">More Info</a>
                     </div>
                 </div>
             </div>
