d5669f9927761c7470f78cd9617a869f86cf4562
Bruno <bruaguspons@gmail.com>
2024-02-02 08:28:23 -0300

    header responsive

--- src/components/contact/ContactInput.tsx
+++ src/components/contact/ContactInput.tsx
@@ -23,38 +23,38 @@ const ContactInput = ({ id, label, autocomplete, type, required, errors }: Props
 
     }
   return (
-    <div className="flex flex-col w-96">
-    <label className="mb-2 text-neutral-100" htmlFor={id}>
-        {label}
-        {required && <span className="text-red-500"> *</span>}
-        {" :"}
-    </label>
-    {
-        type === "textarea" ? (
-            <textarea
-                id={id}
-                name={id}
-                className="focus:outline-none bg-background-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-background-400 dark:border-background-600 dark:placeholder-neutral-400 dark:text-neutral-100 dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                required={required ?? false}
-                placeholder={label}
-                rows={5}
-                onBlur={handleBlur}
-            />
-        ) : (
-            <input
-                id={id}
-                name={id}
-                type={type ?? "text"}
-                autoComplete={autocomplete}
-                className="focus:outline-none bg-background-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-background-400 dark:border-background-600 dark:placeholder-neutral-400 dark:text-neutral-100 dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                required={required?? false}
-                placeholder={label}
-                onBlur={handleBlur}
-            />
-        )
-    }
-    <span className="text-red-500 my-2" style={{display:'none'}} id={`${id}-error`}></span>
-</div>
+    <div className="flex flex-col w-full">
+        <label className="mb-2 text-neutral-100" htmlFor={id}>
+            {label}
+            {required && <span className="text-red-500"> *</span>}
+            {" :"}
+        </label>
+        {
+            type === "textarea" ? (
+                <textarea
+                    id={id}
+                    name={id}
+                    className="focus:outline-none bg-background-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-background-400 dark:border-background-600 dark:placeholder-neutral-400 dark:text-neutral-100 dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    required={required ?? false}
+                    placeholder={label}
+                    rows={5}
+                    onBlur={handleBlur}
+                />
+            ) : (
+                <input
+                    id={id}
+                    name={id}
+                    type={type ?? "text"}
+                    autoComplete={autocomplete}
+                    className="focus:outline-none bg-background-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-background-400 dark:border-background-600 dark:placeholder-neutral-400 dark:text-neutral-100 dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    required={required?? false}
+                    placeholder={label}
+                    onBlur={handleBlur}
+                />
+            )
+        }
+        <span className="text-red-500 my-2" style={{display:'none'}} id={`${id}-error`}></span>
+    </div>
   )
 }
 export default ContactInput
