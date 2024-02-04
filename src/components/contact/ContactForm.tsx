b8045a1d2ae5eab4cd59d9e7be41d6b239a00a10
Bruno <bruaguspons@gmail.com>
2024-02-04 11:24:53 -0300

    fixing last commit

--- src/components/contact/ContactForm.tsx
+++ src/components/contact/ContactForm.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import { useRef } from "react";
 import ButtonSubmit from "./ButtonSubmit.tsx";
 import ContactInput from "./ContactInput.tsx";
 import type { InputError } from "./types/index.ts";
