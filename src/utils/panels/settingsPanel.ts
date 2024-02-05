9b7a1642fbe47cfa71c48e771b4ae4936f7046de
Bruno <bruaguspons@gmail.com>
2024-02-05 08:35:22 -0300

    fixing more errors

--- src/utils/panels/settingsPanel.ts
+++ src/utils/panels/settingsPanel.ts
@@ -1,24 +1,25 @@
 export const showSettingsPanel = () => {
-    const button = document.getElementById("btn-settings");
+    const buttons:NodeListOf<HTMLElement> = document.querySelectorAll(".btn-settings");
     const settings = document.getElementById("header-settings");
-    if (settings && settings.classList.contains("-translate-y-full") && button) {
+    if (settings && settings.classList.contains("-translate-y-full") && buttons.length) {
         settings.classList.remove("-translate-y-full");
         settings.classList.add("translate-y-14");
         settings.classList.add("shadow-2xl");
-        button.dataset.open = "true";
+        buttons.forEach((button) =>button.dataset.open = "true");
+        
     }
 
 }
 export const hideSettingsPanel = () => {
 
     const headerSettings = document.getElementById("header-settings");
-    const button = document.getElementById("btn-settings");
+    const buttons:NodeListOf<HTMLElement> = document.querySelectorAll(".btn-settings");
 
     //settings
-    if (headerSettings && headerSettings.classList.contains("translate-y-14") && button) {
+    if (headerSettings && headerSettings.classList.contains("translate-y-14") && buttons.length) {
         headerSettings.classList.remove("translate-y-14");
         headerSettings.classList.add("-translate-y-full");
         headerSettings.classList.remove("shadow-2xl");
-        button.dataset.open = "false";
+        buttons.forEach((button) =>button.dataset.open = "false");
     }
 }
