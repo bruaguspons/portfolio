e9df97b5e857d4dbe714914ad3f2ea7cd469e0bf
Bruno <bruaguspons@gmail.com>
2024-02-25 09:15:39 -0300

    fixing form

--- src/components/contact/ButtonSubmit.tsx
+++ src/components/contact/ButtonSubmit.tsx
@@ -5,7 +5,7 @@ interface Props{
 const ButtonSubmit = ({children}:Props) => {
   return (
     <button
-            className={`relative inline-flex items-center justify-center p-0.5 overflow-hidden font-medium text-background-600 rounded-lg group bg-gradient-to-br from-purple-500 to-blue-500 focus:outline-none active:from-purple-600 active:to-blue-700 transform active:scale-90 transition-transform cursor-pointer my-4 sm:hover:text-background-100 active:text-background-100 `}
+            className={`relative inline-flex items-center justify-center p-0.5 overflow-hidden font-medium rounded-lg group bg-gradient-to-br from-purple-500 to-blue-500 focus:outline-noneactive:from-purple-600 active:to-blue-700 transform active:scale-90 transition-transform cursor-pointer my-4 `}
             type="submit"
         >
             <span className="relative px-5 py-2.5 transition-all ease-in-out duration-150 bg-background-600 rounded-md w-max sm:group-hover:bg-transparent group-active:bg-transparent">
--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -21,10 +21,10 @@ export const getI18N = ({
 export const getLangFromUrl = (url: URL) => {
     const urlParts = url.pathname.split('/');
 
-    const lenguageArr = Object.entries(LANGUAGES).map(item => item[1] as string)
+    const languageArr = Object.entries(LANGUAGES).map(item => item[1] as string)
 
     for(const part of urlParts){
-      if (lenguageArr.includes(part)) return part as LANGUAGES;
+      if (languageArr.includes(part)) return part as LANGUAGES;
     }
     return defaultLang
 }
